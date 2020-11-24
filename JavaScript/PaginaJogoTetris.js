41359287311d430023a3aade0ec034decd3c0651
Felipe Vedoveto <vedoveto99@gmail.com>
2020-11-24 19:22:51 -0300

    Girando primeira peça

--- JavaScript/PaginaJogoTetris.js
+++ JavaScript/PaginaJogoTetris.js
@@ -30,8 +30,6 @@ class Pecas
   }
 }
 
-document.onkeydown = function(e) { movimentarPecas(e) };
-
 let pecas = new Pecas();
 
 function criarTabelaTetris()
@@ -60,6 +58,8 @@ function criarTabelaTetris()
     
     document.getElementById('divTabelaTetris').appendChild(table);
 
+    document.onkeydown = function(e) { movimentarPecas(e) };
+
     adicionarPecasTabela();    
 
     // BOTÃO PARA BAIXO
@@ -71,7 +71,7 @@ function criarTabelaTetris()
 function adicionarPecasTabela()
 {
     let numeroPeca = Math.floor(Math.random() * 7) + 1;   
-    numeroPeca = 4;
+    numeroPeca = 1;
 
     pecas.pecas = [];
 
@@ -204,6 +204,12 @@ function adicionarPecasTabela()
 
 function movimentarPecas(e)
 {
+
+  if (e.keyCode == 38)
+  {
+    girarPecas(pecas);
+  }
+
   let bodyTabelaTetris = document.getElementById('tableTabelaTetris');    
   let pecaChegouFim = false;
   let pecaColidiu = false;
@@ -212,8 +218,19 @@ function movimentarPecas(e)
   let colunaAuxiliar = null;
   
   pecaColidiu = verificaSeVaiColidir(e.keyCode)
-  console.log(e.keyCode);
-  console.log(pecas)
+
+  if (pecas.pecas[0].numeroPeca == 11)
+  {
+    if (e.keyCode == 37 && pecas.pecas[0].coluna > pecas.pecas[1].coluna)  
+    {      
+      pecas.retornarTodasPecas().reverse();
+    }
+  
+    if (e.keyCode == 39 && pecas.pecas[0].coluna < pecas.pecas[1].coluna)  
+    {
+      pecas.retornarTodasPecas().reverse();
+    }
+  }
 
   if (pecas.pecas[0].numeroPeca == 5)
   {
@@ -282,66 +299,63 @@ function movimentarPecas(e)
     if (!pecaChegouFim)
       pecaChegouFim = linhaNova == 19;
 
-    if ((linhaNova < 20) && (colunaNova >= 0 && colunaNova < 10))
-    {
-      if (!pecaColidiu)
+      if ((linhaNova < 20) && (colunaNova >= 0 && colunaNova < 10))
       {
-        let pecaHTML = bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML;
-          
-        bodyTabelaTetris.rows[linhaNova].cells[colunaNova].innerHTML = pecaHTML;        
-
-        //console.log("1 - " + peca.linha + " " + peca.coluna)
-
-        if (pecaAuxiliar == null && bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.contains("blockPecaAtual"))
+        if (!pecaColidiu)
         {
-          pecaAuxiliar = peca;
-          linhaAuxiliar = linhaAntiga;
-          colunaAuxiliar = colunaAntiga;
+          let pecaHTML = bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML;
+            
+          bodyTabelaTetris.rows[linhaNova].cells[colunaNova].innerHTML = pecaHTML;
+  
+          if (pecaAuxiliar == null && bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.contains("blockPecaAtual"))
+          {
+            pecaAuxiliar = peca;
+            linhaAuxiliar = linhaAntiga;
+            colunaAuxiliar = colunaAntiga;
+          }
+          else
+          {
+            bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML = null;
+          }
+  
+          bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.add('blockPecaAtual');
+          bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].classList.remove('blockPecaAtual');
+  
+          peca.linha = linhaNova;
+          peca.coluna = colunaNova;
+  
+          if (peca.numeroPeca != 5 && peca.numeroPeca != 6)
+          {          
+            if (pecaAuxiliar != null && peca != pecaAuxiliar)
+            {
+              let pecaHTML = bodyTabelaTetris.rows[linhaAuxiliar].cells[colunaAuxiliar].innerHTML;
+              
+              bodyTabelaTetris.rows[pecaAuxiliar.linha].cells[pecaAuxiliar.coluna].innerHTML = pecaHTML;
+              bodyTabelaTetris.rows[linhaAuxiliar].cells[colunaAuxiliar].innerHTML = null;
+  
+              bodyTabelaTetris.rows[pecaAuxiliar.linha].cells[pecaAuxiliar.coluna].classList.add('blockPecaAtual');
+              bodyTabelaTetris.rows[linhaAuxiliar].cells[colunaAuxiliar].classList.remove('blockPecaAtual');
+  
+              pecaAuxiliar = null;
+            }
+          }
+        }
+        else if (e.keyCode == 40)
+        {        
+          pecaChegouFim = true;
         }
         else
         {
-          bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML = null;
+          break;
         }
-
-        bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.add('blockPecaAtual');
-        bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].classList.remove('blockPecaAtual');
-
-        peca.linha = linhaNova;
-        peca.coluna = colunaNova;
-
-        if (peca.numeroPeca != 5 && peca.numeroPeca != 6)
-        {
-        
-          if (pecaAuxiliar != null && peca != pecaAuxiliar)
-          {            
-            let pecaHTML = bodyTabelaTetris.rows[linhaAuxiliar].cells[colunaAuxiliar].innerHTML;
-            
-            bodyTabelaTetris.rows[pecaAuxiliar.linha].cells[pecaAuxiliar.coluna].innerHTML = pecaHTML;
-            bodyTabelaTetris.rows[linhaAuxiliar].cells[colunaAuxiliar].innerHTML = null;
-
-            bodyTabelaTetris.rows[pecaAuxiliar.linha].cells[pecaAuxiliar.coluna].classList.add('blockPecaAtual');
-            bodyTabelaTetris.rows[linhaAuxiliar].cells[colunaAuxiliar].classList.remove('blockPecaAtual');
-
-            pecaAuxiliar = null;
-          }
-        }
-      }
-      else if (e.keyCode == 40)
-      {        
-        pecaChegouFim = true;
-      }
-      else
-      {
-        break;
       }
     }
-  }
 
   if (pecaChegouFim)
   {
     for (let qtdPecas = 0; qtdPecas < pecas.quantidadePecas(); qtdPecas++)
     {    
-      let peca = pecas.pecas[qtdPecas];   
+      let peca = pecas.pecas[qtdPecas];
 
       bodyTabelaTetris.rows[peca.linha].cells[peca.coluna].classList.add('blockPecaTabuleiro');
       bodyTabelaTetris.rows[peca.linha].cells[peca.coluna].classList.remove('blockPecaAtual');
@@ -351,9 +365,39 @@ function movimentarPecas(e)
   }
 }
 
+function realizarRotacaoPeca(linhaAntiga, colunaAntiga, linhaNova, colunaNova, peca)
+{
+  let pecaHTML = bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML;
+          
+  bodyTabelaTetris.rows[linhaNova].cells[colunaNova].innerHTML = pecaHTML;
+
+  if (pecaAuxiliar == null && bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.contains("blockPecaAtual"))
+  {    
+    console.log("?")
+    pecaAuxiliar = peca;
+    linhaAuxiliar = linhaAntiga;
+    colunaAuxiliar = colunaAntiga;
+
+    console.log("k " + linhaAuxiliar)
+  }
+  else
+  {
+    bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML = null;
+  }
+
+  bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.add('blockPecaAtual');
+  bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].classList.remove('blockPecaAtual');
+
+  peca.linha = linhaNova;
+  peca.coluna = colunaNova;
+}
+
 function verificaSeVaiColidir(keyCode)
 {
-  let pecaColidiu = false;  
+  let pecaColidiu = false;
+
+  if (keyCode != 37 && keyCode != 38 && keyCode != 39 && keyCode != 40)  
+    return false;
 
   for (let qtdPecas = 0; qtdPecas < pecas.quantidadePecas(); qtdPecas++)
   {
@@ -363,16 +407,25 @@ function verificaSeVaiColidir(keyCode)
     let colunaAntiga = peca.coluna;
 
     let {linhaNova, colunaNova} = adicionaPosicaoPeca(keyCode, linhaAntiga, colunaAntiga);    
-
-    if (!pecaColidiu)
-      pecaColidiu = bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.contains("blockPecaTabuleiro");
+    
+    if ((linhaNova < 20) && (colunaNova >= 0 && colunaNova < 10))
+    {      
+      if (!pecaColidiu)
+        pecaColidiu = bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.contains("blockPecaTabuleiro");
+    }
+    else
+    {
+      pecaColidiu = true;
+    }    
   }
 
-  return pecaColidiu
+  return pecaColidiu;
 }
 
 function adicionaPosicaoPeca(keyCode, linhaAntiga, colunaAntiga)
 {
+  let linhaNova = -1;
+  let colunaNova = -1;
 
   switch (keyCode)
   {
@@ -389,13 +442,114 @@ function adicionaPosicaoPeca(keyCode, linhaAntiga, colunaAntiga)
       colunaNova = colunaAntiga;      
       break;
     default:
-      return;
+      break;
   }
 
   return {
     linhaNova : linhaNova,
     colunaNova: colunaNova
   };
+}
+
+function girarPecas(pecas)
+{
+  for (let qtdPecas = 0; qtdPecas < pecas.quantidadePecas(); qtdPecas++)
+  {  
+    switch (pecas.pecas[0].numeroPeca)
+    {
+      case 1:
+      case 11:
+        if (qtdPecas == 0)
+        {
+          let pecaAuxiliar = pecas.pecas[0];
+          pecas.pecas[0] = pecas.pecas[3];
+          pecas.pecas[3] = pecaAuxiliar;
+
+          pecaAuxiliar = pecas.pecas[1];
+          pecas.pecas[1] = pecas.pecas[2];
+          pecas.pecas[2] = pecaAuxiliar;          
+        }
+
+        if (pecas.pecas[0].linha == pecas.pecas[3].linha)
+        {
+          let peca = pecas.pecas[qtdPecas];
+
+          let linhaAntiga = peca.linha;
+          let colunaAntiga = peca.coluna;
+
+          let linhaNova = peca.linha + qtdPecas;
+          let colunaNova = pecas.pecas[0].coluna;
+
+          let pecaHTML = bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML;
+
+          console.log(pecaHTML);
+
+          bodyTabelaTetris.rows[linhaNova].cells[colunaNova].innerHTML = pecaHTML;
+          if (linhaNova != linhaAntiga)
+            bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML = null;
+
+          bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.add('blockPecaAtual');
+          bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].classList.remove('blockPecaAtual');
+
+          peca.linha = linhaNova;
+          peca.coluna = colunaNova;
+
+          peca.numeroPeca = 1;
+        }
+        else
+        {          
+          let peca = pecas.pecas[qtdPecas];
+
+          let linhaAntiga = peca.linha;
+          let colunaAntiga = peca.coluna;
+
+          let linhaNova = pecas.pecas[0].linha;
+          let colunaNova = peca.coluna + qtdPecas;
+
+          let pecaHTML = bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML;
+
+          bodyTabelaTetris.rows[linhaNova].cells[colunaNova].innerHTML = pecaHTML;
+          if (colunaNova != colunaAntiga)
+            bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].innerHTML = null;
+
+          bodyTabelaTetris.rows[linhaNova].cells[colunaNova].classList.add('blockPecaAtual');
+          bodyTabelaTetris.rows[linhaAntiga].cells[colunaAntiga].classList.remove('blockPecaAtual');
+
+          peca.linha = linhaNova;
+          peca.coluna = colunaNova;
+
+          peca.numeroPeca = 11;
+        }
+
+        if (qtdPecas == 3)
+        {
+          let pecaAuxiliar = pecas.pecas[3];
+          pecas.pecas[3] = pecas.pecas[0];
+          pecas.pecas[0] = pecaAuxiliar;
+
+          pecaAuxiliar = pecas.pecas[2];
+          pecas.pecas[2] = pecas.pecas[1];
+          pecas.pecas[1] = pecaAuxiliar;          
+        }
+        break;
+      case 2:
+        console.log("GirarPeca2")
+        break;
+      case 3:
+        console.log("GirarPeca2")
+        break;
+      case 4:
+        console.log("GirarPeca2")
+        break;
+      case 5:
+        console.log("GirarPeca2")
+        break;
+      case 6:
+        console.log("GirarPeca2")
+        break;
+      
+    }
+  }
 }
 
 class AppLayout{
