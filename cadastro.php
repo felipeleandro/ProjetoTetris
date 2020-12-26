0818a37a287f34845be8fcfdaf82e697522c6087
Juan Santos <36035455+juansantos99@users.noreply.github.com>
2020-12-26 11:13:48 -0300

    commit

---
+++ cadastro.php
@@ -0,0 +1,45 @@
+<?php
+
+$login = $_POST['nome'];
+$data_nascimento = $_POST['data_nascimento'];
+$telefone = $_POST['telefone'];
+$cpf = $_POST['cpf'];
+$email = $_POST['email'];
+$senha = MD5($_POST['senha']);
+
+$connect = mysql_connect('127.0.0.1','root','');
+$db = mysql_select_db('tetris');
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysql_query($query_select,$connect);
+$array = mysql_fetch_array($select);
+$logarray = $array['login'];
+
+  if($login == "" || $login == null){
+    echo"<script language='javascript' type='text/javascript'>
+    alert('O campo login deve ser preenchido');window.location.href='
+    cadastro.html';</script>";
+
+    }else{
+      if($logarray == $login){
+
+        echo"<script language='javascript' type='text/javascript'>
+        alert('Esse login já existe');window.location.href='
+        cadastro.html';</script>";
+        die();
+
+      }else{
+        $query = "INSERT INTO usuarios (login,data_nascimento,telefone,cpf,email,senha) VALUES ('$login','$data_nascimento','$telefone','$cpf','$email','$senha')";
+        $insert = mysql_query($query,$connect);
+
+        if($insert){
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Usuário cadastrado com sucesso!');window.location.
+          href='login.html'</script>";
+        }else{
+          echo"<script language='javascript' type='text/javascript'>
+          alert('Não foi possível cadastrar esse usuário');window.location
+          .href='cadastro.html'</script>";
+        }
+      }
+    }
+?>
